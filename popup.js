b79523e9f1acaea255f518c5c9bd5aa65bd04d94
FelipeHRDS <felipehenryresende@gmail.com>
2026-09-03 21:32:45 -0300

    Inclusão da função de editar/adicionar o nome de usuario

--- popup.js
+++ popup.js
@@ -8,6 +8,25 @@ function adicionarItem() {
     novoItem.classList.remove("ocultar");
 
     const lixeira = novoItem.querySelector(".fa-trash");
+    const lapis = novoItem.querySelector(".fa-pencil");
+
+    lapis.addEventListener("click", () => {
+        const texto = novoItem.querySelector(".switchText");
+        const input = document.createElement("input");
+
+        input.value = texto.innerText;
+        novoItem.insertBefore(input, texto)
+        texto.remove();
+
+        input.addEventListener("blur", () => {
+            const novoTexto = document.createElement("span");
+            novoTexto.innerText = input.value;
+            novoItem.insertBefore(novoTexto, input);
+            input.remove();
+            novoTexto.classList.add("switchText");
+        });
+    });
+
 
     lixeira.addEventListener("click", (event) => {
         event.target.closest(".grupoUsuario").remove();
@@ -18,6 +37,17 @@ function adicionarItem() {
     switchButton.addEventListener("change", (e) => {
         const isChecked = e.target.checked;
 
+
+        const todosOsSwitches = document.querySelectorAll(".switchInput");
+        
+        if (isChecked) {
+            for (let i = 0; i < todosOsSwitches.length; i++){
+                if (todosOsSwitches[i] !== e.target){
+                    todosOsSwitches[i].checked = false;
+                }
+            }
+        }
+
         console.log(isChecked);
     });
 
@@ -25,3 +55,4 @@ function adicionarItem() {
 }
 
 botao.addEventListener("click", adicionarItem);
+
